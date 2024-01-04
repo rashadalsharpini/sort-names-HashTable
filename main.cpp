137ab2467efd2147b4665fa87df78d867e9216c4
rashadalsharpini <rashadalsharpini@gmail.com>
2024-01-04 16:25:25 +0200

    vector and heap problem

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 #include"HashTable.h"
 #include"student.h"
 using namespace std;
@@ -13,14 +14,34 @@ int main() {
 //    cout<<endl;
 //    h.print();
     cout << "\n\nHello, World!" << endl;
-    student stu[4];
-    for(auto & i : stu)
-        cin>>i;
-    sort(stu,stu+4,student::compareByName);
-    for(const auto & i : stu)
-        cout<<i;
-    sort(stu,stu+4,student::compareById);
-    for(const auto & i : stu)
-        cout<<i;
+//    student stu[4];
+//    for(auto & i : stu)
+//        cin>>i;
+//    sort(stu,stu+4,student::compareByName);
+//    for(const auto & i : stu)
+//        cout<<i;
+//    sort(stu,stu+4,student::compareById);
+//    for(const auto & i : stu)
+//        cout<<i;
+//    vector<int>arr;
+//    for(int i = 0; i < 4 ; i++) {
+//        int value;cin >> value;
+//        arr.push_back(value);
+//    }
+//    for(int i = 0; i < 4 ; i++)
+//        cout << arr[i] << " ";
+//    for(int i = 0; i < 4 ; i++) {
+//        int value;cin >> value;
+//        arr.push_back(value);
+//    }
+//    for(int i = 0 ; i < 8 ; i++)
+//        cout << arr[i] << " ";
+    int size;cin>>size;
+//    int * arr=new int[size];
+    string arr[size];
+    for(auto &i : arr)
+        cin >> i;
+    for(const auto &i : arr)
+        cout << i<< " ";
     return 0;
 }
